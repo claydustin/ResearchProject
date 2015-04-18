344cb30ce87047430166acbbbca1e41183693619
claydustin <dustincm@g.cofc.edu>
2015-04-18 12:47:34 -0400

    changed distance class setup

--- Scripts/run_all.R
+++ Scripts/run_all.R
@@ -1,18 +1,21 @@
 #read in data and select data separated at a specific grain size
-data = read.csv('./data/cross_comms.csv')
-grainSize = 156.25
+data = read.csv('./data/oosting_comms.csv')
+grainSize = 256
 data = data[which(data$grain==grainSize),]
 species.RichnessMatrix = as.matrix(data[which(data$grain==grainSize),-(1:3)])
+
+#global species variance
+Var = sum(cov(species.RichnessMatrix))
 
 #create the distance matrix between sites
 xyCoords = data[,2:3]
 dist.Mat = dist(xyCoords)*sqrt(grainSize)
 
 #create a list of distance classes 
-H <- ceiling(dist.Mat/sqrt(grainSize))*sqrt(grainSize)
+H <- round(dist.Mat/sqrt(grainSize))*sqrt(grainSize)
 hmax <- round((max(dist.Mat/2)/sqrt(grainSize))*sqrt(grainSize))
 H[H>hmax] <- max(H)
-dist.Classes = unique(H)
+dist.Classes = unique(H[-which(H == max(H), arr.ind = TRUE)])
 H <- as.matrix(H)
 
 #separate site pairs according to distance between them 
@@ -28,6 +31,7 @@ for(i in 1:length(dist.Classes)){
     for(j in 1:nrow(diff)){
         cov.Mat[[i]] = cov.Mat[[i]] + diff[j,]%*%t(diff[j,])
     }
+    cov.Mat[[i]] = (1/(2*nrow(sites.ByDist[[i]])))*cov.Mat[[i]]
 }
 
 #create the overall covariance matrix used in PCA
@@ -39,9 +43,9 @@ eigen.values<-eigen.ForC$values
 eigen.vectors<-eigen.ForC$vectors
 
 #partition eigenvalue according to amount explained at distance H
-weighted.Eigenvalues = list()
+weighted.Eigenvalues = c()
 for(i in 1:length(cov.Mat)){
-    weighted.Eigenvalue = t(eigen.vectors[,1])%*%cov.Mat[[i]]%*%eigen.vectors[,1]
+    weighted.Eigenvalue[i] = t(eigen.vectors[,1])%*%cov.Mat[[i]]%*%eigen.vectors[,1]
 }
 
 #Compute the variance of complementarity at each H
@@ -51,16 +55,19 @@ PCAremoved.Cov = c()
 for(i in 1:length(dist.Classes)){
     comp.Cov[i] = sum(diag(cov.Mat[[i]]))
     SR.Cov[i] = sum(cov.Mat[[i]])
-    PCAremoved.Cov[i] = sum(cov.Mat[[i]])-weighted.Eigenvalue[i]
+    PCAremoved.Cov[i] = sum(comp.Cov[i])-weighted.Eigenvalue[i]
 }
 variances = list(comp.Cov, SR.Cov, PCAremoved.Cov)
 
 #Plot the difference variance measures
-plot(dist.Classes, variances[[1]],pch = 15)
+plot(dist.Classes, variances[[1]],pch = 15) #squares
 par(new = T)
-plot(dist.Classes, variances[[2]], pch = 16)
+plot(dist.Classes, variances[[2]], pch = 16) #circles
 par(new = T)
 plot(dist.Classes, variances[[3]], pch = 17)
+par(new = T)
+abline(h = Var)
+
 legend("bottomright", legend = c("Species richness", "Interspecific associations", "Species composition", "Environmental heterogeneity", "without PCA 1", "Single-species aggregation", "global variance without PCA 1"), pch = c(15, 16, 17), bty = 'n')
 
 
