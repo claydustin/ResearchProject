c146e7d68cecb11de657d0be9d36f70938427a7c
claydustin <dustincm@g.cofc.edu>
2015-04-21 13:18:32 -0400

    run_all runs from functions in functions.R

--- Scripts/functions.R
+++ Scripts/functions.R
@@ -1,7 +1,81 @@
-plotVariogram <- function(variances, dists){
-    
-}
 
-plotShading <- function(variances1, variances2, dists){
-      
+mso2 <- function(data, xyCoords, plot = TRUE, grainSize){
+    #read in data and select data separated at a specific grain size
+    object = list()
+    object$data = data
+    object$xy = xyCoords
+    
+    data = data[which(data$grain==grainSize),]
+    abundance.bySpecies = apply(data, 2, sum)
+    species.RichnessMatrix = replace(data, list = which(data > 0,arr.ind = TRUE), 1)
+    
+    #create the distance matrix between sites
+    dist.Mat = dist(xyCoords)*sqrt(grainSize)
+    
+    #create a list of distance classes 
+    H <- round(dist.Mat/sqrt(grainSize))*sqrt(grainSize)
+    hmax <- round((max(dist.Mat/2)/sqrt(grainSize))*sqrt(grainSize))
+    H[H>hmax] <- max(H)
+    dist.Classes = unique(H[-which(H == max(H), arr.ind = TRUE)])
+    H <- as.matrix(H)
+    
+    #separate site pairs according to distance between them 
+    sites.ByDist = list()
+    for(i in 1:length(dist.Classes))
+        sites.ByDist[[i]] = which(H == dist.Classes[i], arr.ind = TRUE)
+    
+    #create a covariance matrix at each distance in H
+    cov.Mat = list()
+    for(i in 1:length(dist.Classes)){
+        diff = species.RichnessMatrix[sites.ByDist[[i]][,1],]-species.RichnessMatrix[sites.ByDist[[i]][,2],]
+        cov.Mat[[i]] = matrix(0, nrow = ncol(diff), ncol = ncol(diff))
+        for(j in 1:nrow(diff)){
+            cov.Mat[[i]] = cov.Mat[[i]] + as.matrix(diff[j,])%*%as.matrix(t(diff[j,]))
+        }
+        cov.Mat[[i]] = (1/(2*nrow(sites.ByDist[[i]])))*cov.Mat[[i]]
+    }
+    
+    #create the overall covariance matrix used in PCA and find the global variance 
+    C = Reduce('+',cov.Mat)
+    global.Var = (1/length(dist.Classes))*sum(C)
+    object$global.Var = global.Var
+    
+    #perform PCA analysis
+    eigen.ForC<-eigen(C)
+    eigen.values<-eigen.ForC$values
+    eigen.vectors<-eigen.ForC$vectors
+    
+    #partition eigenvalue according to amount explained at distance H
+    weighted.Eigenvalues = c()
+    for(i in 1:length(cov.Mat)){
+        weighted.Eigenvalues[i] = t(eigen.vectors[,1])%*%cov.Mat[[i]]%*%eigen.vectors[,1]
+    }
+    
+    #Compute the variance of complementarity at each H. I'm getting the 3rd variance estimate as being comp.Cov -weightedEigenvalues "The observed variance of complementarity without axis 1 appeared tooscillate around its global variance, indicating that all larger scale trend had been accounted for by removing PCA axis 1."
+    comp.Cov = c()
+    SR.Cov = c()
+    PCAremoved.Cov = c()
+    for(i in 1:length(dist.Classes)){
+        comp.Cov[i] = sum(diag(cov.Mat[[i]]))
+        SR.Cov[i] = sum(cov.Mat[[i]])
+        PCAremoved.Cov[i] = sum(comp.Cov[i])-weighted.Eigenvalue[i]
+    }
+    variances = list(comp.Cov, SR.Cov, PCAremoved.Cov)
+    
+    if(plot){
+        #Plot the diffence variance measures
+        xrange = range(dist.Classes)
+        yrange = range(variances)
+        plotchar = c(15:17)
+        
+        plot(xrange, yrange, type = "n", xlab = "Distance", ylab = "Variance")
+        for (i in 1:3){
+            lines(dist.Classes, variances[[i]], type = "b", lwd = 1.5, pch = plotchar[i])
+        }
+        abline(h = global.Var, lty= 4)
+        
+        
+        legend("bottomright", legend = c("Species composition", "Interspecific associations", "Species richness without PCA 1"), pch = c(15, 16, 17), bty = 'n')
+    }
+    return(object)
 }
--- Scripts/run_all.R
+++ Scripts/run_all.R
@@ -1,13 +1,28 @@
+source('../Scripts/functions.R')
+
+#setwd('../../data')
+data.FileNames = grep("comms", list.files(pattern = "\\.csv$"), value = T)
+grainSizes = c(39.06,61.04,39.06,75.56,33.42,39.06,39.06,39.06,87.89,39.06,16.5,61.04,64,56.25,0.25,3.06,39.06,76.56,19.69)
+data = list()
+for(i in 1:length(data.FileNames))
+    data[[i]] = read.csv(data.FileNames[i])
+
+msoList = list()
+for(i in 1:length(data))
+    msoList[[i]] = mso2(data[[i]][,-c(1:3)], data[[i]][,c(2:3)],grainSizes[i])
+
+
 #read in data and select data separated at a specific grain size
-data = read.csv('./data/oosting_comms.csv')
+data = read.csv('./oosting_comms.csv')
 grainSize = 256
 data = data[which(data$grain==grainSize),]
-species.RichnessMatrix = as.matrix(data[which(data$grain==grainSize),-(1:3)])
-abundance.bySpecies = apply(species.RichnessMatrix, 2, sum)
+species.RichnessMatrix = as.matrix(data[,-(1:3)])
+abundance.bySpecies = apply(species.RichnessMatrix, 2, sum)#this one
 species.RichnessMatrix = species.RichnessMatrix[,which(abundance.bySpecies>5,arr.ind = TRUE)]
+species.RichnessMatrix = replace(species.RichnessMatrix, list = which(species.RichnessMatrix > 0,arr.ind = TRUE), 1)#and this one
 
 #global species variance
-Var = sum(cov(species.RichnessMatrix))
+#Var = sum(cov(species.RichnessMatrix))
 
 #create the distance matrix between sites
 xyCoords = data[,2:3]
@@ -36,8 +51,9 @@ for(i in 1:length(dist.Classes)){
     cov.Mat[[i]] = (1/(2*nrow(sites.ByDist[[i]])))*cov.Mat[[i]]
 }
 
-#create the overall covariance matrix used in PCA
+#create the overall covariance matrix used in PCA and find the global variance 
 C = Reduce('+',cov.Mat)
+Var = (1/length(dist.Classes))*sum(C)
 
 #perform PCA analysis
 eigen.ForC<-eigen(C)
@@ -45,12 +61,12 @@ eigen.values<-eigen.ForC$values
 eigen.vectors<-eigen.ForC$vectors
 
 #partition eigenvalue according to amount explained at distance H
-weighted.Eigenvalues = c()
+weighted.Eigenvalue = c()
 for(i in 1:length(cov.Mat)){
     weighted.Eigenvalue[i] = t(eigen.vectors[,1])%*%cov.Mat[[i]]%*%eigen.vectors[,1]
 }
 
-#Compute the variance of complementarity at each H
+#Compute the variance of complementarity at each H. I'm getting the 3rd variance estimate as being comp.Cov -weightedEigenvalues "The observed variance of complementarity without axis 1 appeared tooscillate around its global variance, indicating that all larger scale trend had been accounted for by removing PCA axis 1."
 comp.Cov = c()
 SR.Cov = c()
 PCAremoved.Cov = c()
@@ -64,15 +80,14 @@ variances = list(comp.Cov, SR.Cov, PCAremoved.Cov)
 #Plot the diffence variance measures
 xrange = range(dist.Classes)
 yrange = range(variances)
-linetype = c(2:5)
 plotchar = c(15:17)
 
 plot(xrange, yrange, type = "n", xlab = "Distance", ylab = "Variance")
 for (i in 1:3){
-    lines(dist.Classes, variances[[i]], type = "b", lwd = 1.5, lty = linetype[i], pch = plotchar[i])
+    lines(dist.Classes, variances[[i]], type = "b", lwd = 1.5, pch = plotchar[i])
 }
+abline(h = Var, lty= 4)
 
 
 legend("bottomright", legend = c("Species richness", "Interspecific associations", "Species composition", "Environmental heterogeneity", "without PCA 1", "Single-species aggregation", "global variance without PCA 1"), pch = c(15, 16, 17), bty = 'n')
-
 
